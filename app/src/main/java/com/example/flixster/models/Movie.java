b4f90f9970d4d338b64755bffedc62797f327828
Daniel Condly <dcond007@fiu.edu>
2021-01-24 19:46:03 -0500

    Initial commit

---
+++ app/src/main/java/com/example/flixster/models/Movie.java
@@ -0,0 +1,57 @@
+
+package com.example.flixster.models;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+// Movie class encapsulates an idea of a movie.
+public class Movie {
+
+    String backdropPath;
+    String posterPath;
+    String title;
+    String overview;
+
+    public Movie (JSONObject jsonObject) throws JSONException {
+        backdropPath = jsonObject.getString("backdrop_path");
+        posterPath = jsonObject.getString("poster_path");
+        title = jsonObject.getString("title");
+        overview = jsonObject.getString("overview");
+
+    }
+
+    // Iterating through the Json Array and constructing a movie,
+    // for each element in this json array.
+    public static List<Movie> fromJsonArray(JSONArray movieJsonArray) throws JSONException {
+        List<Movie> movies = new ArrayList<>();
+
+        for (int i = 0; i < movieJsonArray.length(); i++) {
+            movies.add(new Movie(movieJsonArray.getJSONObject(i)));
+        }
+        return movies;
+    }
+
+    // Get data out of these objects via getter methods
+
+    public String getPosterPath() {
+        // Edit this to return full url.
+        // hardcode the url with a width of 342.
+        return String.format("https://image.tmdb.org/t/p/w342/%s", posterPath);
+    }
+
+    public String getBackdropPath() {
+        return String.format("https://image.tmdb.org/t/p/w342/%s", backdropPath);
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getOverview() {
+        return overview;
+    }
+}
